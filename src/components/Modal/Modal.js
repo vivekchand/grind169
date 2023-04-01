8dfcf99300a4e4fa2f02169e05412a035ce1378b
Vivek Chand <vivekchand19@gmail.com>
2023-04-02 00:12:16 +0200

    update background color

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,11 @@
 import './Modal.css';
 
-const Modal = ({ handleClose, handleComplete, show, children, isCompleted }) => {
+const Modal = ({ handleClose, handleComplete, show, children, background, isCompleted }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
   const buttonText = isCompleted ? "Mark as not clear" : "Mark as understood";
   return (
-    <div className={showHideClassName}>
-      <section className="modal-main">
+    <div className={showHideClassName} style={ {backgroundColor: background} }>
+      <section className="modal-main" style={ {backgroundColor: background} }>
         {children}
         <button className="close-btn" type="button" onClick={handleClose}>
           &times;
