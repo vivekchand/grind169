a666eb9f67a9cf5460e7152566bb23aebddebace
Vivek Chand <vivekchand19@gmail.com>
2024-10-09 23:15:56 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({ completedCount, easyCount, mediumCount, hardCount, totalCount }) => {
   return (
     <div className='header'>
       <h1>GrindCode</h1>
+      <div className="metrics">
+        <p>Completed: {completedCount} / {totalCount}</p>
+        <p>Easy: {easyCount} | Medium: {mediumCount} | Hard: {hardCount}</p>
+      </div>
+      <div className="motivation">
+        <p>Keep going! You're making great progress. Every problem solved is a step towards mastery!</p>
+      </div>
     </div>
   )
 }
