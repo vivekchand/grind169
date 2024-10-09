0273e8b9adb54ed1a00f9d2fb3baf68285c786a7
Vivek Chand <vivekchand19@gmail.com>
2024-10-09 23:14:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import React from "react";
 import './App.css'
 
 import CardsList from "./components/CardsList";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <Header />
       <CardsList />
     </div>
   );
